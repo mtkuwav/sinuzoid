b63ed16dee95a3edd8e67086fdc957bdd845f1d1
matioku <math.chaulab@gmail.com>
2025-05-27 22:49:48 +0200

    feat: add SVGR plugin and new logos, update App and Footer components
    - Added `vite-plugin-svgr` to package.json for SVG handling.
    - Added new SVG logos: `logo_sinuzoid-cyan.svg` and `logo_sinuzoid_text-cyan.svg`.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/common/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-cyan-500">    Hello world!  </h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/common/footer.tsx
@@ -0,0 +1,11 @@
+function Test() {
+  return (
+    <h1 className="text-3xl font-bold underline text-cyan-500">  Hello world!  </h1>
+  )
+}
+
+export default function Footer() {
+  return (
+    <Test />
+  )
+}
